7824e219809131a5fa97a6f407ef6fb8203f2948
greed-d <deekshit.bti@gmail.com>
2025-06-17 11:49:23 +0545

    `chore` : add qmlls for qml

--- lsp/ruff.lua
+++ lsp/ruff.lua
@@ -1,4 +1,3 @@
-local blink = require("blink.cmp")
 return {
 	cmd = { "ruff" },
 	filetypes = { "python" },
@@ -9,16 +8,10 @@ return {
 	settings = {
 		ruff = {},
 	},
-	capabilities = vim.tbl_deep_extend(
-		"force",
-		{},
-		vim.lsp.protocol.make_client_capabilities(),
-		blink.get_lsp_capabilities(),
-		{
-			fileOperations = {
-				didRename = true,
-				willRename = true,
-			},
-		}
-	),
+	capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), {
+		fileOperations = {
+			didRename = true,
+			willRename = true,
+		},
+	}),
 }
