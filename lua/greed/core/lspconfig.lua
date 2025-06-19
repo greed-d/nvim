1e010d909b8240807c90417d507aa6216a85252b
greed-d <deekshit.bti@gmail.com>
2025-06-19 12:29:25 +0545

    feat : add dot as virtual text indicator

--- lua/greed/core/lspconfig.lua
+++ lua/greed/core/lspconfig.lua
@@ -10,9 +10,15 @@ vim.lsp.enable({
 -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.buf.signature_help({ border = "rounded" })
 
 vim.diagnostic.config({
-	virtual_text = true,
-	underline = true,
-	update_in_insert = false,
+
+	virtual_text = {
+		spacing = 4,
+		source = "if_many",
+		prefix = "●",
+		-- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+		-- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+		-- prefix = "icons",
+	},
 	severity_sort = true,
 	float = {
 		border = "rounded",
