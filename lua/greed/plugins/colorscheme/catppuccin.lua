73bce239379a81d16190234ec02fdf01e2d43c3d
greed-d <deekshit.bti@gmail.com>
2025-09-22 15:00:29 +0545

    fix : color stuff

--- lua/greed/plugins/colorscheme/catppuccin.lua
+++ lua/greed/plugins/colorscheme/catppuccin.lua
@@ -57,8 +57,21 @@ return {
 				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
 			},
 		})
+		require("catppuccin").setup({})
+
+		vim.cmd.colorscheme("catppuccin")
+		local function set_highlight(group, opts)
+			vim.api.nvim_set_hl(0, group, opts)
+		end
+		local mocha = require("catppuccin.palettes").get_palette("mocha")
+		set_highlight("FloatBorder", { fg = mocha.surface2, bg = mocha.base })
+		set_highlight("NormalFloat", { fg = mocha.surface2, bg = mocha.base })
+		set_highlight("SnacksPickerList", { fg = mocha.subtext0, bg = mocha.base })
+		set_highlight("SnacksPickerListTitle", { fg = mocha.crust, bg = mocha.green })
+		set_highlight("SnacksPickerInputTitle", { fg = mocha.crust, bg = mocha.sapphire })
+		set_highlight("SnacksPickerPreviewTitle", { fg = mocha.crust, bg = mocha.blue })
+		set_highlight("SnacksPickerInput", { fg = mocha.subtext0, bg = mocha.base })
 
 		-- setup must be called before loading
-		vim.cmd.colorscheme("catppuccin")
 	end,
 }
