1e0a973c9d84ca3928241eaf3cbc36be6117e900
greed-d <deekshit.bti@gmail.com>
2025-10-18 20:13:07 +0545

    chore : changes and more

--- lua/greed/plugins/colorscheme/vauge.lua
+++ lua/greed/plugins/colorscheme/vauge.lua
@@ -1,6 +1,6 @@
 return {
 	"vague2k/vague.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	lazy = false,   -- make sure we load this during startup if it is your main colorscheme
 	enabled = false,
 	priority = 1000, -- make sure to load this before all the other plugins
 	config = function()
