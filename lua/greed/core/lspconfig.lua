114f5d910e057d3f5b90fb8d85eb94a68e2bf9c2
greed-d <deekshit.bti@gmail.com>
2025-11-09 20:39:58 +0545

    chore : add virtual line for current line only

--- lua/greed/core/lspconfig.lua
+++ lua/greed/core/lspconfig.lua
@@ -23,12 +23,14 @@ vim.lsp.enable(lsp_files)
 vim.diagnostic.config({
 
 	virtual_text = {
+
 		spacing = 4,
 		source = "if_many",
 		prefix = "●",
 		-- this will set set the prefix to a function that returns the diagnostics icon based on the severity
 		-- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
 		-- prefix = "icons",
+		current_line = true,
 	},
 	severity_sort = true,
 	float = {
