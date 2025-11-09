883f5e584c8577ca4902a87b73534e85e4d4961b
greed-d <deekshit.bti@gmail.com>
2025-11-09 20:53:03 +0545

    init : initialize v12 config

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 require("greed.core")
-require("greed.lazy")
+vim.pack.add {
+  { src = 'https://github.com/catppuccin/nvim' },
+  { src = 'https://github.com/mason-org/mason.nvim'},
+  { src = 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim'}
+
+}
+
+vim.cmd('colorscheme catppuccin-mocha')
+require('mason').setup()
+require('mason-tool-installer').setup({
+  ensure_installed = {'lua-language-server', 'basedpyright', 'ruff', 'stylua'}
+})
+-- require("greed.lazy")
