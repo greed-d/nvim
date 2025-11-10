0c35876aa35cb3f0b2ea9e7763ed4ec17a16b569
greed-d <deekshit.bti@gmail.com>
2025-11-10 21:34:07 +0545

    chore : add mason

--- init.lua
+++ init.lua
@@ -1,14 +1,5 @@
 require("core")
-auto_init = require('auto_plugin_init')
-auto_init.init()
-vim.pack.add {
-  { src = 'https://github.com/mason-org/mason.nvim'},
-  { src = 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim'}
+AUTO_INIT = require('auto_plugin_init')
 
-}
-
-require('mason').setup()
-require('mason-tool-installer').setup({
-  ensure_installed = {'lua-language-server', 'basedpyright', 'ruff', 'stylua'}
-})
+AUTO_INIT.init()
 -- require("greed.lazy")
