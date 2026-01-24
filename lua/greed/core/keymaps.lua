7e91ef1974afe14e14355a440e927ad22f991664
greed-d <deekshit.bti@gmail.com>
2026-01-24 15:43:47 +0545

    chore : update keymaps

--- lua/greed/core/keymaps.lua
+++ lua/greed/core/keymaps.lua
@@ -41,10 +41,10 @@ map("v", ">", ">gv", opts)
 -- [[ Code stuff ]]
 
 -- [[ Tabs Stuff ]]
-map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
-map("n", "<leader>tk", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
-map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })                     --  go to next tab
-map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
+map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+map("n", "]t", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+map("n", "[t", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- [[ Buffer Stuff ]]
