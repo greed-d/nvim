958f085f26cb7d8d4463fb3c48d4c90eb368beb2
greed-d <deekshit.bti@gmail.com>
2026-02-02 16:03:20 +0545

    fix(deps) : add missing dependency for mini.ai

--- lua/plugins/mini/init.lua
+++ lua/plugins/mini/init.lua
@@ -1,5 +1,8 @@
 vim.pack.add({
 	{ src = "https://github.com/nvim-mini/mini.nvim" },
+
+	-- Dependencies :
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
 })
 
 require("mini.comment").setup({})
