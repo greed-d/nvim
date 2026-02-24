a11617c4c0935344b0f5f6633c838dff22029736
greed-d <deekshit.bti@gmail.com>
2026-02-24 14:46:41 +0545

    chore(config) : add floating config and commit keybind

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -5,4 +5,15 @@ vim.pack.add({
 	"https://github.com/esmuellert/codediff.nvim",
 })
 
+require("neogit").setup({
+	floating = {
+		relative = "editor",
+		width = 0.9,
+		height = 0.8,
+		style = "minimal",
+		border = "rounded",
+	},
+})
 map("n", "<leader>gg", "<cmd>Neogit kind=floating<cr>")
+map("n", "<leader>gP", "<cmd>Neogit push<cr>")
+map("n", "<leader>gc", "<cmd>Neogit commit<cr>")
