61a28a6e213ab54d60614090551a08cfc48bee83
greed-d <deekshit.bti@gmail.com>
2026-04-09 15:28:34 +0545

    add(cord) : add presence to neovim

---
+++ lua/plugins/cord.lua
@@ -0,0 +1,11 @@
+vim.pack.add({ "https://github.com/vyfor/cord.nvim" })
+
+require("cord").setup({
+	display = {
+		theme = "catppuccin",
+		flavor = "dark",
+		view = "full",
+		swap_fields = false,
+		swap_icons = false,
+	},
+})
