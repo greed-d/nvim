dbddd199b477bd3f469d8c450fc1945cb4d2b700
greed-d <deekshit.bti@gmail.com>
2026-07-29 16:57:47 +0545

    chore : update github url

--- lua/plugins/tmux-navigation.lua
+++ lua/plugins/tmux-navigation.lua
@@ -1,4 +1,4 @@
-vim.pack.add({ "gh:alexghergh/nvim-tmux-navigation" })
+vim.pack.add({ "https://github.com/alexghergh/nvim-tmux-navigation" })
 
 local nvim_tmux_nav = require("nvim-tmux-navigation")
 nvim_tmux_nav.setup({
