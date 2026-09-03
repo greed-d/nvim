2bec89ffe1e719cae74cef631e79271a3a697676
greed-d <deekshit.bti@gmail.com>
2026-09-03 21:12:57 +0545

    chore : go formatter

--- lua/core/lsp/confom.lua
+++ lua/core/lsp/confom.lua
@@ -34,12 +34,18 @@ require("conform").setup({
 		http = { "kulala-fmt" },
 		sh = { "shellharden" },
 		kdl = { "kdlfmt" },
-		go = { "gofmt" },
+		go = { "gofmt", "goimports-reviser" },
 		-- Conform can also run multiple formatters sequentially
 
 		-- python = { "isort", "black" },
 		--
 		-- You can use 'stop_after_first' to run the first available formatter from the list
 		-- javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+	formatters = {
+		["goimports-reviser"] = {
+			cwd = require("conform.util").root_file({ "go.mod" }),
+			require_cwd = true,
+		},
 	},
 })
